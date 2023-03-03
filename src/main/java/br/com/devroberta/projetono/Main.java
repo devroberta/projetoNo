56be25590a9462f088d92fef70165220bfa55301
Roberta de Siqueira <designer.roberta@gmail.com>
2023-03-03 16:59:42 -0300

    Refactor Generics Type

--- src/main/java/br/com/devroberta/projetono/Main.java
+++ src/main/java/br/com/devroberta/projetono/Main.java
@@ -5,19 +5,19 @@ public class Main {
     public static void main(String[] args) {
 
         //instanciando um nó
-        No no1 = new No("Conteúdo no1");
-        No no2 = new No("Conteúdo no2");
+        No<String> no1 = new No<String>("Conteúdo no1");
+        No<String> no2 = new No<String>("Conteúdo no2");
 
         //apontando a referência no no1 para o no2
         no1.setProximoNo(no2);
         System.out.println("OBJETO no1 {\n  Conteúdo: " + no1.getConteudo() + "\n  Referência: " + no1.getProximoNo() + "\n}\n");
 
-        No no3 = new No("Conteúdo no3");
+        No<String> no3 = new No<String>("Conteúdo no3");
 
         //apontando a referência no no2 para o no3
         no2.setProximoNo(no3);
 
-        No no4 = new No("Conteúdo no4");
+        No<String> no4 = new No<String>("Conteúdo no4");
 
         //apontando a referência no no3 para o no4
         no3.setProximoNo(no4);
